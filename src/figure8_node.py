b15d3c98c6573e6c48689e74bfe33dd887ce8225
Blake_Strebel <blakestrebel2018@u.northwestern.edu>
2016-10-07 20:31:12 -0500

    Turle teleports to initial position moves straight

--- src/figure8_node.py
+++ src/figure8_node.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.srv import TeleportAbsolute
+
 ## Import message types here, dont forget to update package.cml and Cmakelist
 
+def teleport_absolute_client(x, y, theta):
+    rospy.wait_for_service('/turtle1/teleport_absolute')
+    try:
+        teleport_absolute = rospy.ServiceProxy('/turtle1/teleport_absolute',TeleportAbsolute)
+        teleport_absolute(x, y, theta)
+    except rospy.ServiceException,e:
+        print "Service call failed: %s" %e
 
-def move_pub():
+def control_turtle():
     pub = rospy.Publisher('turtle1/cmd_vel',Twist, queue_size=10)
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
         trajectory = Twist()
 
         trajectory.linear.x = 1
-        trajectory.linear.y = 0
+        trajectory.linear.y = 1
         trajectory.linear.z = 0
         trajectory.angular.x = 0
-        trajectory.angular.y = 0
+        trajectory.angular.y = 1
         trajectory.angular.y = 0
 
         pub.publish(trajectory)
@@ -24,6 +33,7 @@ def move_pub():
 if __name__ == '__main__':
     rospy.init_node('figure8_node')
     try:
-        move_pub()
+        teleport_absolute_client(6, 6, 0)
+        control_turtle()
     except rospy.ROSInterruptException:
         pass
